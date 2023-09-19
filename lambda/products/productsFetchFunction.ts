d2cd7b68238f7924d79fd652dd367869e396eb55
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-09-18 22:54:31 -0300

    feat: implementation of initial productsFetchFunction

---
+++ lambda/products/productsFetchFunction.ts
@@ -0,0 +1,25 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+
+export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+
+    const method = event.httpMethod
+    if (event.resource === "/products") {
+        if (method === 'GET') {
+            console.log('GET')
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: "GET Products - OK",
+                })
+            }
+        }
+    }
+
+    return {
+        statusCode: 400,
+        body: JSON.stringify({
+            message: "Bad request"
+        })
+    }
+}
