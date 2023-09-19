4ecf5f8ea705b7b1eefaf282578aa6c9fc85ffda
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-09-18 23:01:51 -0300

    feat: create simple log messages for lambda exec

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -1,6 +1,10 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+    const lambdaRequestId = context.awsRequestId
+    const apiRequestId = event.requestContext.requestId
+
+    console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`)
 
     const method = event.httpMethod
     if (event.resource === "/products") {
