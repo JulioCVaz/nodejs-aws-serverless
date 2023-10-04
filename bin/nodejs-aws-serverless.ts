ef4792bd1d94dd6395eff88ad7033240773c6e82
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-10-04 01:33:59 -0300

    feat: create lambda layer and initialize instance

--- bin/nodejs-aws-serverless.ts
+++ bin/nodejs-aws-serverless.ts
@@ -4,6 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 
 import { ProductsAppStack } from '../lib/productsApp-stack';
 import { EcommerceApiStack } from '../lib/ecommerceApi-stack';
+import { ProductsAppLayersStack } from '../lib/productsAppLayers-stack'
 
 const app = new cdk.App();
 
@@ -18,10 +19,17 @@ const tags = {
   team: "SiecolaCode"
 }
 
+const productsAppLayersStack = new ProductsAppLayersStack(app, "ProductsAppLayers", {
+  tags,
+  env
+})
+
 const productsAppStack = new ProductsAppStack(app, "ProductsApp", {
   tags,
   env
 })
+
+productsAppStack.addDependency(productsAppLayersStack)
 
 const ecommerceApiStack = new EcommerceApiStack(app, "EcommerceApi", {
   productsFetchHandler: productsAppStack.productsFetchHandler,
