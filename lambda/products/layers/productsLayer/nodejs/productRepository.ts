e265b7dad422991af31b81f991db7e69a5d31678
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-10-24 22:42:05 -0300

    feat: implement ordersFunction with handler

--- lambda/products/layers/productsLayer/nodejs/productRepository.ts
+++ lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -43,6 +43,24 @@ export class ProductRepository {
         }
     }
 
+    async getProductsByIds(productIds: string[]): Promise<Product[]> {
+        const keys: {id: string; }[] = []
+        productIds.forEach((productId) => {
+            keys.push({
+                id: productId
+            })
+        })
+        const data = await this.ddbClient.batchGet({
+            RequestItems: {
+                [this.productsDdb]: {
+                    Keys: keys
+                },
+            }
+        }).promise()
+
+        return data.Responses![this.productsDdb] as Product[]
+    }
+
     async create(product: Product): Promise<Product> {
         product.id = uuid()
         await this.ddbClient.put({
--- lib/ordersApp-stack.ts
+++ lib/ordersApp-stack.ts
@@ -34,6 +34,10 @@ export class OrdersAppStack extends cdk.Stack {
       const ordersLayerArn = ssm.StringParameter.valueForStringParameter(this, "OrdersLayerVersionArn")
       const ordersLayer = lambda.LayerVersion.fromLayerVersionArn(this, "OrdersLayerVersionArn", ordersLayerArn)
 
+      // Orders Api Layer
+      const ordersApiLayerArn = ssm.StringParameter.valueForStringParameter(this, "OrdersApiLayerVersionArn")
+      const ordersApiLayer = lambda.LayerVersion.fromLayerVersionArn(this, "OrdersApiLayerVersionArn", ordersApiLayerArn)
+
       // Products Layer
       const productsLayerArn = ssm.StringParameter.valueForStringParameter(this, "ProductsLayerVersionArn")
       const productsLayer = lambda.LayerVersion.fromLayerVersionArn(this, "ProductsLayerVersionArn", productsLayerArn)
@@ -53,7 +57,7 @@ export class OrdersAppStack extends cdk.Stack {
             PRODUCTS_DDB: props.productsDdb.tableName,
             ORDERS_DDB: ordersDdb.tableName
         },
-        layers: [ordersLayer, productsLayer],
+        layers: [ordersLayer, productsLayer, ordersApiLayer],
         tracing: lambda.Tracing.ACTIVE,
         insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
       })
