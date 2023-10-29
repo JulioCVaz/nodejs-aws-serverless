4b492e2ac80930a3f4e6c2f5327c0037d8fc8875
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-10-29 16:02:38 -0300

    feat: initialize lambdaEvent into stack

--- bin/nodejs-aws-serverless.ts
+++ bin/nodejs-aws-serverless.ts
@@ -49,11 +49,13 @@ const ordersAppLayersStack = new OrdersAppLayersStack(app, "OrdersAppLayers", {
 const ordersAppStack = new OrdersAppStack(app, "OrdersApp", {
   tags,
   env,
-  productsDdb: productsAppStack.productsDdb
+  productsDdb: productsAppStack.productsDdb,
+  eventsDdb: eventsDdbStack.table,
 })
 
 ordersAppStack.addDependency(productsAppStack)
 ordersAppStack.addDependency(ordersAppLayersStack)
+ordersAppStack.addDependency(eventsDdbStack)
 
 const ecommerceApiStack = new EcommerceApiStack(app, "EcommerceApi", {
   productsFetchHandler: productsAppStack.productsFetchHandler,
