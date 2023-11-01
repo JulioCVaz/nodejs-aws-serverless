9515a33f79bec9e192505d0e0911991cd93b6d15
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-10-31 23:26:58 -0300

    fix: re enable config batching to orderemails handler

--- lib/ordersApp-stack.ts
+++ lib/ordersApp-stack.ts
@@ -191,11 +191,11 @@ export class OrdersAppStack extends cdk.Stack {
         insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
       })
 
-      orderEmailsHandler.addEventSource(new lambdaEventSource.SqsEventSource(orderEventsQueue/*, {
+      orderEmailsHandler.addEventSource(new lambdaEventSource.SqsEventSource(orderEventsQueue, {
         batchSize: 5,
         enabled: true,
         maxBatchingWindow: cdk.Duration.minutes(1)
-      }*/))
+      }))
       orderEventsQueue.grantConsumeMessages(orderEmailsHandler)
     }
 }
