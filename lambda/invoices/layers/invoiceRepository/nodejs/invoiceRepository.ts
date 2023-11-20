e3249a0af9961cf93a17eaa3dfea0d6b3f32326b
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-11-20 19:47:22 -0300

    feat: improve invoice transactions and create repository

--- lambda/invoices/layers/invoiceRepository/nodejs/invoiceRepository.ts
+++ lambda/invoices/layers/invoiceRepository/nodejs/invoiceRepository.ts
@@ -1,4 +1,39 @@
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+
+export interface InvoiceFile {
+    customerName: string;
+    invoiceNumber: string;
+    totalValue: number;
+    productId: string;
+    quantity: number;
+}
+
 export interface Invoice {
     pk: string;
     sk: string;
+    totalValue: number;
+    productId: string;
+    quantity: number;
+    transactionId: string;
+    ttl: number;
+    createdAt: number;
+}
+
+export class InvoiceRepository {
+    private ddbClient: DocumentClient
+    private invoicesDdb: string
+
+    constructor(ddbClient: DocumentClient, invoicesDdb: string) {
+        this.ddbClient = ddbClient
+        this.invoicesDdb = invoicesDdb
+    }
+
+    async create(invoice: Invoice): Promise<Invoice> {
+        await this.ddbClient.put({
+            TableName: this.invoicesDdb,
+            Item: invoice
+        }).promise()
+
+        return invoice
+    }
 }
