8bc4b6090220cf5c834966eb4d725af321b8b7b6
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-11-20 19:47:40 -0300

    feat: ensure disconnect client websocket

--- lambda/invoices/layers/invoiceWSConnection/nodejs/invoiceWSConnection.ts
+++ lambda/invoices/layers/invoiceWSConnection/nodejs/invoiceWSConnection.ts
@@ -23,4 +23,29 @@ export class InvoiceWSService {
             return false
         }
     }
+
+    sendInvoiceStatus(transactionId: string, connectionId: string, status: string) {
+        const postData = JSON.stringify({
+            transactionId: transactionId,
+            status: status
+        })
+
+        return this.sendData(connectionId, postData)
+    }
+
+    async disconnectClient(connectionId: string): Promise<boolean> {
+        try {
+            await this.apiGatewayManagementApi.getConnection({
+                ConnectionId: connectionId
+            }).promise()
+
+            await this.apiGatewayManagementApi.deleteConnection({
+                ConnectionId: connectionId
+            }).promise()
+
+            return true
+        } catch (error) {
+            return false
+        }
+    }
 }
