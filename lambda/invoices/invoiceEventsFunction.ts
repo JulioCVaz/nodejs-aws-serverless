fb13904bd13d56031a501cc658b44155b7e74a97
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-11-23 00:37:37 -0300

    feat: add process to timeout transaction if is deleted or ttl

--- lambda/invoices/invoiceEventsFunction.ts
+++ lambda/invoices/invoiceEventsFunction.ts
@@ -28,7 +28,10 @@ export async function handler(event: DynamoDBStreamEvent, context: Context): Pro
         } else if (record.eventName === "MODIFY") {
 
         } else if (record.eventName === "REMOVE") {
-
+            if (record.dynamodb!.OldImage!.pk.S!.startsWith("#transaction")) {
+                console.log("Invoice transaction event received")
+                promises.push(processExpiredTransaction(record.dynamodb!.OldImage!))
+            }
         }
     })
 
@@ -58,3 +61,20 @@ async function createEvent(invoiceImage: { [key: string]: AttributeValue }, even
 
     return
 }
+
+// process to timeout and disconnectClient and implemented with ttl
+async function processExpiredTransaction(invoiceTransactionImage: { [key: string]: AttributeValue; }): Promise<void> {
+    const transactionId = invoiceTransactionImage.sk.S!
+    const connectionId = invoiceTransactionImage.connectionId.S!
+
+    console.log(`Transaction ${transactionId} - ConnectionId: ${connectionId}`)
+
+    if (invoiceTransactionImage.transactionStatus.S === 'INVOICE_PROCESSED') {
+        console.log("Invoice processed")
+    } else {
+        console.log(`Invoice import failed - Status: ${invoiceTransactionImage.transactionStatus.S}`)
+        await invoiceWSService.sendInvoiceStatus(transactionId, connectionId, "TIMEOUT")
+        await invoiceWSService.disconnectClient(connectionId)
+    }
+}
+
