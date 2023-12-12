d1562b3ee9338207202ef03a84500f73d806ba96
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-12-11 23:17:46 -0300

    feat: implement authLayerStack and layer

---
+++ lib/authLayers-stack.ts
@@ -0,0 +1,22 @@
+import * as cdk from "aws-cdk-lib"
+import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as ssm from "aws-cdk-lib/aws-ssm"
+import { Construct } from "constructs"
+
+export class AuthLayersStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props)
+    
+        const authUserInfoLayer = new lambda.LayerVersion(this, "AuthUserInfoLayer", {
+            code: lambda.Code.fromAsset('lambda/auth/layers/authUserInfo'),
+            compatibleRuntimes: [lambda.Runtime.NODEJS_16_X],
+            layerVersionName: "AuthUserInfo",
+            removalPolicy: cdk.RemovalPolicy.RETAIN
+        })
+
+        new ssm.StringParameter(this, "AuthUserInfoLayerVersionArn", {
+            parameterName: "AuthUserInfoLayerVersionArn",
+            stringValue: authUserInfoLayer.layerVersionArn
+        })
+    }
+}
