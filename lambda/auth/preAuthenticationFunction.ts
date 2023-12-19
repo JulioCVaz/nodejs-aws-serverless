02a306908e164338d02c9cb15200fcb55e1f97ee
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-12-18 22:32:04 -0300

    feat: add example of block user in preAuth

--- lambda/auth/preAuthenticationFunction.ts
+++ lambda/auth/preAuthenticationFunction.ts
@@ -3,5 +3,10 @@ import { Callback, Context, PreAuthenticationTriggerEvent } from "aws-lambda";
 export async function handler(event: PreAuthenticationTriggerEvent, context: Context, callback: Callback): Promise<void> {
     console.log(event)
 
+    // @note example to block user access 
+    // if (event.request.userAttributes.email === "siecola@gmail.com") {
+    //     callback("this user is blocked. Reason: PAYMENT", event)
+    // }
+
     callback(null, event)
 }
